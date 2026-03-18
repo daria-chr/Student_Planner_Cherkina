53e25f302a01da5ec6700a87d7bb304b1368e201
daria-chr <cherkinadar@gmail.com>
2026-03-18 12:49:29 +0300

    tep 2: reate creen sealed class for routes

---
+++ app/src/main/java/com/cherkina/student_planner/navigation/Screen.kt
@@ -0,0 +1,13 @@
+package com.cherkina.student_planner.navigation
+
+import javax.security.auth.Subject
+
+sealed class Screen(val route: String){
+    object Home: Screen("home")
+    object Details: Screen("details/{subjectId}"){
+        fun createRoute(subject: String)="details/$subject"
+    }
+    object Prifile: Screen("profile")
+    object  Settings: Screen("settings")
+
+}
