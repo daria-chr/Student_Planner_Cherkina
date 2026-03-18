fc23a798ad40de5c9ab0cb808407b4fb72d0972b
daria-chr <cherkinadar@gmail.com>
2026-03-18 14:29:28 +0300

    tep 7: etup avost and complete navigation

--- app/src/main/java/com/cherkina/student_planner/ui_model/ProfileScreen.kt
+++ app/src/main/java/com/cherkina/student_planner/ui_model/ProfileScreen.kt
@@ -62,12 +62,12 @@ fun ProfileScreen(
                     )
                     Spacer(modifier = Modifier.height(16.dp))
                     Text(
-                        text = "Иванов Иван Иванович",
+                        text = "Черкина Дарья Алексеевна",
                         style = MaterialTheme.typography.headlineSmall,
                         fontWeight = FontWeight.Bold
                     )
                     Text(
-                        text = "Группа: ИСП-234",
+                        text = "Группа: ИСП-231",
                         style = MaterialTheme.typography.titleMedium,
                         color = MaterialTheme.colorScheme.onPrimaryContainer
                     )
@@ -88,7 +88,7 @@ fun ProfileScreen(
                     ProfileInfoRow(
                         icon = Icons.Default.Email,
                         label = "Email",
-                        value = "ivanov@vgi.volsu.ru"
+                        value = "cherkina@gmail.com"
                     )
                     HorizontalDivider()
                     ProfileInfoRow(
